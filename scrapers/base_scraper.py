9d0ecab6cb4cec392ddb61bc740cda9948f4e71f
Amansharma105 <sharmaaman87600@gmail.com>
2026-07-14 16:58:26 +0530

    Add BaseScraper class with page fetching and parsing

--- scrapers/base_scraper.py
+++ scrapers/base_scraper.py
@@ -1,1 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
 
+class BaseScraper:
+
+    def fetch_page(self, url):
+        response = requests.get(url)
+        return response.text
+
+    def parse_html(self, html):
+        return BeautifulSoup(html, "html.parser")
