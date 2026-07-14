1e397ce5cc7b9a06cfe4eef76df0f83ab6f66a5b
Amansharma105 <sharmaaman87600@gmail.com>
2026-07-14 17:31:27 +0530

    Add price checking job to scheduler

--- scheduler.py
+++ scheduler.py
@@ -1,1 +1,8 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
 
+scheduler = BlockingScheduler()
+
+def check_prices():
+    print("Checking product prices...")
+
+scheduler.add_job(check_prices, "interval", hours=1)
