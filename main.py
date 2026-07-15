8811be0363a0b91b1ea571004a44997495349617
Amansharma105 <sharmaaman87600@gmail.com>
2026-07-15 17:36:09 +0530

    Implement price analysis in analyze command
    Added price analysis functionality to the analyze command.

--- main.py
+++ main.py
@@ -1,46 +1,62 @@
 import typer
+from analysis.price_analysis import analyze_prices
 
 app = typer.Typer()
+
 
 @app.command()
 def start():
     print("WebScrapePro is running!")
 
+
 @app.command()
 def scrape():
     print("Scraping product data...")
+
 
 @app.command()
 def report():
     print("Generating report...")
 
+
 @app.command()
 def analyze():
-    print("Price analysis completed!")
+    prices = [49999, 47999, 48999, 46999]
+
+    result = analyze_prices(prices)
+
+    print(result)
+
 
 @app.command()
 def chart():
     print("Generating price charts...")
 
+
 @app.command()
 def alert():
     print("Sending price alerts...")
+
 
 @app.command()
 def dashboard():
     print("Launching dashboard...")
 
+
 @app.command()
 def html():
     print("Generating HTML report...")
+
 
 @app.command()
 def schedule():
     print("Scheduler started...")
 
+
 @app.command()
 def test():
     print("Running tests...")
+
 
 if __name__ == "__main__":
     app()
