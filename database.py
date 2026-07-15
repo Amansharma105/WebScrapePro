87db245dcb6d6a3b3fe001b87b2182eea67ec96a
Amansharma105 <sharmaaman87600@gmail.com>
2026-07-15 17:31:07 +0530

    Refactor database connection to use constant name

--- database.py
+++ database.py
@@ -1,4 +1,26 @@
 import sqlite3
 
+
+DATABASE_NAME = "products.db"
+
+
 def create_connection():
-    return sqlite3.connect("products.db")
+    return sqlite3.connect(DATABASE_NAME)
+
+
+def create_table():
+    conn = create_connection()
+
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS products(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT,
+        price TEXT,
+        website TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
