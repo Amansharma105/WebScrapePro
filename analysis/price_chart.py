1af7a62fd757436f99205f0f54c7286028631a16
Amansharma105 <sharmaaman87600@gmail.com>
2026-07-15 17:33:59 +0530

    Improve line_chart visualization and save functionality
    Enhanced line_chart function with figure size and markers.

--- analysis/price_chart.py
+++ analysis/price_chart.py
@@ -1,9 +1,33 @@
 import matplotlib.pyplot as plt
 
+
 def line_chart(prices):
-    plt.plot(prices)
+
+    plt.figure(figsize=(8,4))
+
+    plt.plot(prices, marker="o")
+
     plt.title("Price Trend")
 
+    plt.xlabel("Days")
+
+    plt.ylabel("Price")
+
+    plt.grid(True)
+
+    plt.savefig("data/line_chart.png")
+
+    plt.close()
+
+
 def bar_chart(products, prices):
+
+    plt.figure(figsize=(8,4))
+
     plt.bar(products, prices)
+
     plt.title("Product Prices")
+
+    plt.savefig("data/bar_chart.png")
+
+    plt.close()
