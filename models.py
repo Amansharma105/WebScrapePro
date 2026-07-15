118c8775e79b2b300da46e7a0fe496d16a91836b
Amansharma105 <sharmaaman87600@gmail.com>
2026-07-15 17:32:41 +0530

    Rename 'name' to 'title' in Product class

--- models.py
+++ models.py
@@ -1,5 +1,13 @@
 class Product:
-    def __init__(self, name, price, website):
-        self.name = name
+
+    def __init__(self, title, price, website):
+
+        self.title = title
+
         self.price = price
+
         self.website = website
+
+    def __str__(self):
+
+        return f"{self.title} - {self.price} ({self.website})"
