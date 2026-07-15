e63476deefc9931bb998bacc84a10911fb66ea31
Amansharma105 <sharmaaman87600@gmail.com>
2026-07-15 17:33:13 +0530

    Refactor price analysis functions for clarity

--- analysis/price_analysis.py
+++ analysis/price_analysis.py
@@ -1,8 +1,19 @@
-def lowest_price(prices):
+def get_lowest_price(prices):
     return min(prices)
 
-def highest_price(prices):
+
+def get_highest_price(prices):
     return max(prices)
 
-def average_price(prices):
-    return sum(prices) / len(prices)
+
+def get_average_price(prices):
+    return round(sum(prices) / len(prices), 2)
+
+
+def analyze_prices(prices):
+
+    return {
+        "Lowest Price": get_lowest_price(prices),
+        "Highest Price": get_highest_price(prices),
+        "Average Price": get_average_price(prices)
+    }
