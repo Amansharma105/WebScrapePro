7f465f4859c62fa3f09f4bfbf64b119021b8664c
Amansharma105 <sharmaaman87600@gmail.com>
2026-07-15 17:54:51 +0530

    Refactor job addition for better readability

--- scheduler.py
+++ scheduler.py
@@ -1,8 +1,20 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 
+
 scheduler = BlockingScheduler()
+
 
 def check_prices():
     print("Checking product prices...")
 
-scheduler.add_job(check_prices, "interval", hours=1)
+
+scheduler.add_job(
+    check_prices,
+    "interval",
+    hours=1
+)
+
+
+def start_scheduler():
+    print("Scheduler Started")
+    scheduler.start()
